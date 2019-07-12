fa03bcb20c170011eb9ae06b84d92ab4948349b4
SouVitorGabriel <vitor.s.alencar@hotmai.com>
2019-07-12 16:27:19 -0300

    Criada redoma de orbita da câmera; Criado script da redoma da câmera;

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject redoma;
+
+    public float velocidade = 5f;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float h = velocidade * Input.GetAxis("Mouse X");
+            float v = velocidade * Input.GetAxis("Mouse Y");
+
+            if (redoma.transform.eulerAngles.z + v <= 0.1f || redoma.transform.eulerAngles.z + v >= 179.9f)
+                v = 0;
+
+            redoma.transform.eulerAngles = new Vector3(redoma.transform.eulerAngles.x, redoma.transform.eulerAngles.y + h, redoma.transform.eulerAngles.z + v);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0)
+        {
+            redoma.transform.localScale = redoma.transform.localScale * (1f - scroll);
+        }
+
+    }
+}
